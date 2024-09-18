3653d911645bc8bf76785019e84678a358b033d2
Dan <begalievvvv@gmail.com>
2024-09-17 19:18:27 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import StudentsForm from "./components/custom/StudentsForm";
+
+const App = () => {
+  return (
+    <div>
+      <StudentsForm />
+    </div>
+  );
+};
+export default App;
